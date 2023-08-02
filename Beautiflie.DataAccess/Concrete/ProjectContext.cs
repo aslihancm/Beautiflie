0dbb1b98a93dccc2a3784244115c0672f7b4a245
aslihancm <comu.aslihan@gmail.com>
2023-08-02 22:50:23 +0300

    Add project files.

---
+++ Beautiflie.DataAccess/Concrete/ProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beautiflie.DataAccess.Concrete
+{
+    public class ProjectContext:DbContext
+    {
+
+       protected override void onConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-A5C24TT\SQLEXPRESS; Database=Beautiflie; Integrated Security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Beautiflie.Entities/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beautiflie.Entities
+{
+    public class ProductCategory
+    {
+
+        public int ProductId { get; set; }
+        public Product ProductId { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category CategoryId { get; set; }
+
+
+    }
+}
