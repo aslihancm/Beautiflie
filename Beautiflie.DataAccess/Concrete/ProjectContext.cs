af5c806f099d134f9b6d95238bfebc41103cb737
aslihancm <comu.aslihan@gmail.com>
2023-08-05 11:20:30 +0300

    SqlServer bağlantı

--- Beautiflie.DataAccess/Concrete/ProjectContext.cs
+++ Beautiflie.DataAccess/Concrete/ProjectContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Beautiflie.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +10,11 @@ namespace Beautiflie.DataAccess.Concrete
 {
     public class ProjectContext:DbContext
     {
-
-       protected override void onConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-A5C24TT\SQLEXPRESS; Database=Beautiflie; Integrated Security=true");
         }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
     }
